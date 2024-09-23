1cb8d6ff4d3a9c08cd7f99fcd8d700140e61c789
Diego Donaggio <38855585+ddon-90@users.noreply.github.com>
2024-09-23 09:41:22 +0100

    Update faststore.config.js

--- faststore.config.js
+++ faststore.config.js
@@ -83,7 +83,7 @@ module.exports = {
 
   analytics: {
     // https://developers.google.com/tag-platform/tag-manager/web#standard_web_page_installation,
-    gtmContainerId: "G-96WD1FR3WE",
+    gtmContainerId: "GTM-59ZTPWL9",
   },
 
   experimental: {
